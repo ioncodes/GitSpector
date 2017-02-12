1a3f6ff0a60307ad8e13a10194a83f06a33633e1
Luca Marcelli <ionix@protonmail.com>
2017-02-12 14:55:59 +0100

    added popup and buttons for project add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const fs = require('fs-extra');
 var request = require('then-request');
-var json = fs.readJsonSync(__dirname + '/data/projects.json', {throws: true});
-var settings = fs.readJsonSync(__dirname + '/data/settings.json', {throws: true});
+var json = fs.readJsonSync(__dirname + '/data/projects.json', {
+    throws: true
+});
+var settings = fs.readJsonSync(__dirname + '/data/settings.json', {
+    throws: true
+});
 var authHeader = 'Basic ' + new Buffer(settings.username + ':' + settings.password).toString('base64');
 
 load();
@@ -9,7 +13,7 @@ load();
 function load() {
     var categoryWrap = document.createElement('div');
     categoryWrap.className = 'category-wrap';
-    for(var i = 0; i < json.length; i++) {
+    for (var i = 0; i < json.length; i++) {
         (function() {
             var url = json[i].url;
             var git = json[i].git;
@@ -18,16 +22,16 @@ function load() {
             var head = document.createElement('div');
             head.className = 'head';
             head.innerText = name;
-            if(url !== "") {
-                request('GET', url).done(function (res) {
+            if (url !== "") {
+                request('GET', url).done(function(res) {
                     var body = JSON.parse(res.body.toString('utf-8'));
                     process.nextTick(function() {
-                        if(body.build.status === 'success') {
+                        if (body.build.status === 'success') {
                             success = true;
                         } else {
                             success = false;
                         }
-                        if(success) {
+                        if (success) {
                             var checkmark = document.createElement('i');
                             checkmark.className = 'fa fa-check';
                             checkmark.setAttribute('aria-hidden', 'true');
@@ -43,7 +47,12 @@ function load() {
                 });
             }
 
-            request('GET', git, {headers:{"User-Agent":"Electron","Authorization":authHeader}}).done(function (res) {
+            request('GET', git, {
+                headers: {
+                    "User-Agent": "Electron",
+                    "Authorization": authHeader
+                }
+            }).done(function(res) {
                 var body = JSON.parse(res.body.toString('utf-8'));
                 process.nextTick(function() {
                     var stars = body.stargazers_count;
@@ -58,16 +67,16 @@ function load() {
 
                     statisticWrap.appendChild(head);
 
-                    for(var j = 0; j < 3; j++) {
+                    for (var j = 0; j < 3; j++) {
                         var statistic = document.createElement('div');
                         statistic.className = 'statistic';
 
                         var count = document.createElement('div');
                         count.className = 'count';
-                        if(j === 0) {
+                        if (j === 0) {
                             count.innerText = watchers;
                             console.log(watchers);
-                        } else if(j === 1) {
+                        } else if (j === 1) {
                             count.innerText = stars;
                             console.log(stars);
                         } else {
@@ -77,9 +86,9 @@ function load() {
 
                         var title = document.createElement('div');
                         title.className = 'title';
-                        if(j === 0) {
+                        if (j === 0) {
                             title.innerText = 'Watchers';
-                        } else if(j === 1) {
+                        } else if (j === 1) {
                             title.innerText = 'Stars';
                         } else {
                             title.innerText = 'Forks';
@@ -108,3 +117,25 @@ function load() {
 
     document.getElementById('body').appendChild(nano);
 }
+
+jQuery(document).ready(function($) {
+    //open popup
+    $('.cd-popup-trigger').on('click', function(event) {
+        event.preventDefault();
+        $('.cd-popup').addClass('is-visible');
+    });
+
+    //close popup
+    $('.cd-popup').on('click', function(event) {
+        if ($(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup')) {
+            event.preventDefault();
+            $(this).removeClass('is-visible');
+        }
+    });
+    //close popup when clicking the esc keyboard button
+    $(document).keyup(function(event) {
+        if (event.which == '27') {
+            $('.cd-popup').removeClass('is-visible');
+        }
+    });
+});
